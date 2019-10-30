989f12d288940d58ff93ef7ed3236c1c67502d28
Aliaksandr_Paluyan <Aliaksandr_Paluyan@epam.com>
2019-10-30 16:01:46 +0300

    [Task-1] fix comments

---
+++ src/js/itemRender.js
@@ -0,0 +1,42 @@
+export default (itemInfo, isChannel) => {
+  let item = document.createElement('section');
+
+  if (isChannel) {
+    item.className = 'channel';
+    item.setAttribute('data-url-attribute', itemInfo.id)
+    item.innerHTML = `
+      <div class="channel-name">
+        <strong>${itemInfo.name}</strong>
+      </div>
+      <div class="channel-description">
+        ${itemInfo.description}
+      </div>
+      <div class="channel-category">
+        <i>${itemInfo.category}</i>
+      </div>
+      <a class="link-to-official" href="${itemInfo.url}">
+        Go to official site
+      </a>`
+  } else {
+    const date = new Date(itemInfo.publishedAt);
+    const publishDate = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`;
+
+    item.className = "news-item";
+    item.innerHTML = `
+      <div class="news-title">
+        <strong>${itemInfo.title}</strong>
+      </div>
+      <div class="news-description">
+        ${itemInfo.description}
+      </div>
+      <div class="news-info">
+        <span>Was published on ${publishDate} ${itemInfo.author ? 'by <i>' + itemInfo.author + '</i>' : ''} 
+      </div>
+      ${itemInfo.urlToImage ? '<img class="news-image" src=' + itemInfo.urlToImage + ">" : ''}
+      <a class="link-to-official" href="${itemInfo.url}">
+        Go to official site
+      </a>`
+  }
+
+  return item;
+}
